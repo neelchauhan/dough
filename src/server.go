0c47e3779d2f86bd04482f5212f9fb108d78fa3b
Neel Chauhan <neel@neelc.org>
2019-03-10 16:40:48 -0400

    Add (incomplete) server-side message processing

--- src/server.go
+++ src/server.go
@@ -15,7 +15,16 @@ func (this *handler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
     switch r.Question[0].Qtype {
         case dns.TypeTXT:
             msg.Authoritative = true
-            domain_msg := strings.Replace(msg.Question[0].Name, "." + GlobalSettings.Domain + ".", "", 1)
-            base32.StdEncoding.DecodeString(domain_msg) // TODO: Assign to variable
+            // Remove the specified server's domain from the subdomain (which contains the messages)
+            domainMsg := strings.Replace(msg.Question[0].Name, "." + GlobalSettings.Domain + ".", "", 1)
+            decodedMsg, err := base32.StdEncoding.DecodeString(domainMsg)
+            var outMsg message
+            var outBytes []byte
+            if err != nil {
+                outMsg = msg_checksum_invalid{0}
+                outBytes = msg_to_bytes(outMsg, MSG_SIZE_RECV_TXT)
+            } else {
+                domainMsg := bytes_to_msg(decodedMsg, uint16(len(decodedMsg)))
+            }
     }
 }
