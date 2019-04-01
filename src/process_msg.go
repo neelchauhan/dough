a47b781f5c9009d5de93f920ee09abe95f125370
Neel Chauhan <neel@neelc.org>
2019-04-01 16:02:24 -0400

    Use handle_conn_init_req() (Type 1 Message) in process_msg()

--- src/process_msg.go
+++ src/process_msg.go
@@ -1,10 +1,13 @@
 package main
 
 func process_msg(msg message) message {
+    var outMsg message
     switch msg.mtype() {
         case MSG_TYPE_CONN_INIT_REQ:
-            ;
+            outMsg = handle_conn_init_req(msg)
         default:
             ;
     }
+
+    return outMsg
 }
