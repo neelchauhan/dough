347efc000ec8fe9af8ad07748708a491bb8497cd
Neel Chauhan <neel@neelc.org>
2019-04-01 16:09:31 -0400

    Check that if we already have a connection specifier, don't allow its reuse

--- src/conn_mgmt.go
+++ src/conn_mgmt.go
@@ -11,6 +11,7 @@ type Connection struct {
 }
 
 var ConnectionTable map[uint32]Connection
+var ConnSpecIdMap map[uint32]uint32
 
 func init_conn_table() {
     ConnectionTable = make(map[uint32]Connection)
@@ -19,9 +20,16 @@ func init_conn_table() {
 func handle_conn_init_req(msg message) message {
     connInitReq := msg_conn_init_req(msg)
 
+    // Reject if we already have this connection ID specifier
+    if _, ok := ConnSpecIdMap[connInitReq.conn_id_spec]; ok {
+        return msg_conn_rejected{2}
+    }
+    // TODO: Reject connections on server overload
+
     newConnId := connInitReq.conn_id_spec * rand.Intn(8)
 
     ConnectionTable[newConnId] = Connection{newConnId, MSG_TYPE_CONN_INIT_REQ, MSG_TYPE_CONN_ACCEPTED}
+    ConnSpecIdMap[connInitReq.conn_id_spec]
 
     return msg_conn_accepted{newConnId}
 }
