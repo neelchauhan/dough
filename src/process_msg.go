b0f4f929aa32ad26a6d381e143542ed25359e092
Neel Chauhan <neel@neelc.org>
2019-04-17 19:27:23 -0400

    Handle send data in process_msg.go

--- src/process_msg.go
+++ src/process_msg.go
@@ -5,6 +5,8 @@ func process_msg_server(msg message) message {
     switch msg.mtype() {
         case MSG_TYPE_CONN_INIT_REQ:
             outMsg = handle_conn_init_req(msg)
+        case MSG_TYPE_CONN_SEND_DATA:
+            outMsg = handle_conn_send_data(msg)
         default:
             ;
     }
