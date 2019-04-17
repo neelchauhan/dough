e8cc8f815e9e6299321cf03611457f01a0ec89a5
Neel Chauhan <neel@neelc.org>
2019-04-17 18:46:05 -0400

    Use correct sequence number fields

--- src/conn_mgmt.go
+++ src/conn_mgmt.go
@@ -53,13 +53,13 @@ func handle_conn_send_data(msg message) message {
 
     conn := ConnectionTable[connSendData.conn_id]
 
-    if connSendData.seq_no + uint32(connSendData.size) != conn.sendSeqNo + uint32(connSendData.size) {
+    if connSendData.seq_no + uint32(connSendData.size) != conn.sendSeqNumber + uint32(connSendData.size) {
         return msg_checksum_invalid{}
     }
 
     connSendData.seq_no = connSendData.seq_no + uint32(connSendData.size)
     if connSendData.seq_no > 2**32 - 1 {
-        connSendData.seq_no = connSendData - 2**32
+        connSendData.seq_no = connSendData.seq_no - 2**32
     }
 
     return msg_conn_send_data_ack{}
